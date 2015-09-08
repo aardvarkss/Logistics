85030eaa9bc25d567d0560dd068dfc196de4c911
aardvarkss <aardvarkss@live.co.uk>
2015-09-08 16:07:19 +0100

    skeleton structure of application, including NUnit tests

---
+++ UnitTest/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class SuccessTests
+    {
+        [SetUp]
+        public void Init()
+        {  }
+
+        [TearDown]
+        public void Dispose()
+        { /* ... */ }
+
+        [Test]
+        public void Test1()
+        { Assert.True(true); }
+    }
+}
---
+++ UnitTest/UsersTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Tests
+{
+    [TestFixture()]
+    public class UsersTests
+    {
+        [Test()]
+        public void getUserNameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
